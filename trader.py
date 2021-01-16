66fe6eb4988f3c753e6e98ea87ee45a5019bd878
Joe Holden <joseph.holden@rocketmail.com>
2021-01-16 13:25:40 +0000

    Now uses a logger instead of just writing to a file

--- trader.py
+++ trader.py
@@ -1,9 +1,9 @@
 import cbpro
 import os
+import logging
 
 
 def buycrypto():
-
     btc_order = auth_client.buy(order_type="market",
                                 product_id='BTC-EUR',
                                 funds="10")  # Amount you want to buy
@@ -26,53 +26,63 @@ def buycrypto():
 
 
 def writetofile(orders):
-
     # Bitcoin Details
     btcdets = auth_client.get_order(orders[0])  # Uses the order ID to get extra, specific details of transaction
 
     try:
-        msg = "Date & Time:\t" + btcdets["created_at"] + "\tGross Spent:\t" + btcdets["specified_funds"] \
-              + "\tFees:\t" + btcdets["fill_fees"] + "\tNet Spent:\t" + btcdets["funds"] + \
-              "\tAmount Bought:\t" + btcdets["filled_size"]
+        msg = "\u20BFitcoin - Date & Time:" + btcdets["created_at"] + " - Gross Spent:" + btcdets["specified_funds"] \
+              + " - Fees:" + btcdets["fill_fees"] + " - Net Spent:" + btcdets["funds"] + \
+              " - Amount Bought:" + btcdets["filled_size"]
     except:
-        msg = "Error printing Bitcoin details"  # Don't want to break the whole program so it prints this instead
+        msg = "Error printing \u20BFitcoin details"  # Don't want to break the whole program so it prints this instead
 
-    f = open(os.environ.get("CoinbaseDirectory") + "btc.txt", "a")
-    f.write(msg + "\n")
-    f.close()
+    logger.info(msg)
 
     # Litecoin Details
     ltcdets = auth_client.get_order(orders[1])
 
     try:
-        msg = "Date & Time:\t" + ltcdets["created_at"] + "\tGross Spent:\t" + ltcdets["specified_funds"] \
-              + "\tFees:\t" + ltcdets["fill_fees"] + "\tNet Spent:\t" + ltcdets["funds"] + \
-              "\tAmount Bought:\t" + ltcdets["filled_size"]
+        msg = "Litecoin - Date & Time:" + ltcdets["created_at"] + " - Gross Spent:" + ltcdets["specified_funds"] \
+              + " - Fees:" + ltcdets["fill_fees"] + " - Net Spent:" + ltcdets["funds"] + \
+              " - Amount Bought:" + ltcdets["filled_size"]
     except:
         msg = "Error printing Litecoin details"
 
-    f = open(os.environ.get("CoinbaseDirectory") + "ltc.txt", "a")
-    f.write(msg + "\n")
-    f.close()
+    logger.info(msg)
 
     # Ethereum Details
     ethdets = auth_client.get_order(orders[2])
 
     try:
-        msg = "Date & Time:\t" + ethdets["created_at"] + "\tGross Spent:\t" + ethdets["specified_funds"] \
-              + "\tFees:\t" + ethdets["fill_fees"] + "\tNet Spent:\t" + ethdets["funds"] + \
-              "\tAmount Bought:\t" + ethdets["filled_size"]
+        msg = "Ethereum - Date & Time:" + ethdets["created_at"] + " - Gross Spent:" + ethdets["specified_funds"] \
+              + " - Fees:" + ethdets["fill_fees"] + " - Net Spent:" + ethdets["funds"] + \
+              " - Amount Bought:" + ethdets["filled_size"]
     except:
         msg = "Error printing Ethereum details"
 
-    f = open(os.environ.get("CoinbaseDirectory") + "eth.txt", "a")
-    f.write(msg + "\n")
-    f.close()
+    logger.info(msg)
+
+
+# ------------------------------------------------------------------
+#   Logging Setup
+# ------------------------------------------------------------------
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(message)s')
+
+file_handler = logging.FileHandler(os.environ.get("CoinbaseDirectory") + "CoinbasePro-Trader.log", encoding='utf8')
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+# ------------------------------------------------------------------
 
 
 auth_client = cbpro.AuthenticatedClient(os.environ.get("CoinbasePro_API_Public"),
-                                            os.environ.get("CoinbasePro_API_Secret"),
-                                            os.environ.get("CoinbasePro_Passphrase"))
+                                        os.environ.get("CoinbasePro_API_Secret"),
+                                        os.environ.get("CoinbasePro_Passphrase"))
 
 orders = buycrypto()
 writetofile(orders)
