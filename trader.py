3c7d1b231cd2c6a9475a0cbc41bdf2210c74c098
Joe Holden <joseph.holden@rocketmail.com>
2021-01-18 15:29:42 +0000

    Now sends a message through a Telegram bot aswell as logging to a .log file

--- trader.py
+++ trader.py
@@ -2,6 +2,7 @@ import cbpro
 import os
 import logging
 import time
+import requests
 
 
 def buycrypto():
@@ -32,54 +33,74 @@ def buycrypto():
     return btc_orderid, ltc_orderid, eth_orderid, link_orderid
 
 
-def writetofile(orders):
+def sendmsg(orders, chatid, bot_token):
     # Bitcoin Details
     btcdets = auth_client.get_order(orders[0])  # Uses the order ID to get extra, specific details of transaction
 
     try:
-        msg = "\u20BFitcoin\t - Date & Time:" + btcdets["created_at"] + " - Gross Spent:" + btcdets["specified_funds"] \
-              + " - Fees:" + btcdets["fill_fees"] + " - Net Spent:" + btcdets["funds"] + \
-              " - Amount Bought:" + btcdets["filled_size"]
+        msg = f'\u20BFitcoin - Date & Time:{btcdets["created_at"]} - Gross Spent: {btcdets["specified_funds"]}' \
+              f' - Fees: {btcdets["fill_fees"]} - Net Spent: {btcdets["funds"]}' \
+              f' - Amount Bought: {btcdets["filled_size"]}'
     except:
-        msg = "Error printing \u20BFitcoin details"  # Don't want to break the whole program so it prints this instead
+        msg = "Error getting \u20BFitcoin details"  # Don't want to break the whole program so it prints this instead
 
     logger.info(msg)
+
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chatid}&text={msg}"
+
+    # send the msg
+    requests.get(url)
 
     # Litecoin Details
     ltcdets = auth_client.get_order(orders[1])
 
     try:
-        msg = "Litecoin - Date & Time:" + ltcdets["created_at"] + " - Gross Spent:" + ltcdets["specified_funds"] \
-              + " - Fees:" + ltcdets["fill_fees"] + " - Net Spent:" + ltcdets["funds"] + \
-              " - Amount Bought:" + ltcdets["filled_size"]
+        msg = f'Litecoin - Date & Time:{ltcdets["created_at"]} - Gross Spent: {ltcdets["specified_funds"]}' \
+              f' - Fees: {ltcdets["fill_fees"]} - Net Spent: {ltcdets["funds"]}' \
+              f' - Amount Bought: {ltcdets["filled_size"]}'
     except:
-        msg = "Error printing Litecoin details"
+        msg = "Error getting Litecoin details"
 
     logger.info(msg)
+
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chatid}&text={msg}"
+
+    # send the msg
+    requests.get(url)
 
     # Ethereum Details
     ethdets = auth_client.get_order(orders[2])
 
     try:
-        msg = "Ethereum - Date & Time:" + ethdets["created_at"] + " - Gross Spent:" + ethdets["specified_funds"] \
-              + " - Fees:" + ethdets["fill_fees"] + " - Net Spent:" + ethdets["funds"] + \
-              " - Amount Bought:" + ethdets["filled_size"]
+        msg = f'Ethereum - Date & Time:{ethdets["created_at"]} - Gross Spent: {ethdets["specified_funds"]}' \
+              f' - Fees: {ethdets["fill_fees"]} - Net Spent: {ethdets["funds"]}' \
+              f' - Amount Bought: {ethdets["filled_size"]}'
     except:
-        msg = "Error printing Ethereum details"
+        msg = "Error getting Ethereum details"
 
     logger.info(msg)
+
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chatid}&text={msg}"
+
+    # send the msg
+    requests.get(url)
 
     # Chainlink Details
     linkdets = auth_client.get_order(orders[3])
 
     try:
-        msg = "Ethereum - Date & Time:" + linkdets["created_at"] + " - Gross Spent:" + linkdets["specified_funds"] \
-              + " - Fees:" + linkdets["fill_fees"] + " - Net Spent:" + linkdets["funds"] + \
-              " - Amount Bought:" + linkdets["filled_size"]
+        msg = f'Chainlink - Date & Time:{linkdets["created_at"]} - Gross Spent: {linkdets["specified_funds"]}' \
+              f' - Fees: {linkdets["fill_fees"]} - Net Spent: {linkdets["funds"]}' \
+              f' - Amount Bought: {linkdets["filled_size"]}'
     except:
-        msg = "Error printing Ethereum details"
+        msg = "Error getting Ethereum details"
 
     logger.info(msg)
+
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chatid}&text={msg}"
+
+    # send the msg
+    requests.get(url)
 
 
 # ------------------------------------------------------------------
@@ -103,6 +124,9 @@ auth_client = cbpro.AuthenticatedClient(os.environ.get("CoinbasePro_API_Public")
                                         os.environ.get("CoinbasePro_API_Secret"),
                                         os.environ.get("CoinbasePro_Passphrase"))
 
+bot_token = os.environ.get("CryptoTrackerBotToken")
+chatid = os.environ.get("TelegramChatID")
+
 orders = buycrypto()
 time.sleep(10)  # Wait 10 seconds for CB to catch up and log the transaction
-writetofile(orders)
+sendmsg(orders, chatid, bot_token)
